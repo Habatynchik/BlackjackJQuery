d61d29d6d58e0b69fa2e09533f59bfb1380268e3
Nikita Gamaiunov <Habatynchik@gmail.com>
2025-03-30 18:21:06 +0300

    Some changes

--- script.js
+++ script.js
@@ -1,340 +1,75 @@
 $(document).ready(function () {
     let deck = [
-        {
-            value: "ace",
-            shortValue: "A",
-            suit: "club",
-            shortSuit: "♣",
-            price: 11,
-        },
-        { value: "2", shortValue: "2", suit: "club", shortSuit: "♣", price: 2 },
-        { value: "3", shortValue: "3", suit: "club", shortSuit: "♣", price: 3 },
-        { value: "4", shortValue: "4", suit: "club", shortSuit: "♣", price: 4 },
-        { value: "5", shortValue: "5", suit: "club", shortSuit: "♣", price: 5 },
-        { value: "6", shortValue: "6", suit: "club", shortSuit: "♣", price: 6 },
-        { value: "7", shortValue: "7", suit: "club", shortSuit: "♣", price: 7 },
-        { value: "8", shortValue: "8", suit: "club", shortSuit: "♣", price: 8 },
-        { value: "9", shortValue: "9", suit: "club", shortSuit: "♣", price: 9 },
-        {
-            value: "10",
-            shortValue: "10",
-            suit: "club",
-            shortSuit: "♣",
-            price: 10,
-        },
-        {
-            value: "jack",
-            shortValue: "J",
-            suit: "club",
-            shortSuit: "♣",
-            price: 10,
-        },
-        {
-            value: "queen",
-            shortValue: "Q",
-            suit: "club",
-            shortSuit: "♣",
-            price: 10,
-        },
-        {
-            value: "king",
-            shortValue: "K",
-            suit: "club",
-            shortSuit: "♣",
-            price: 10,
-        },
-        {
-            value: "ace",
-            shortValue: "A",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 11,
-        },
-        {
-            value: "2",
-            shortValue: "2",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 2,
-        },
-        {
-            value: "3",
-            shortValue: "3",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 3,
-        },
-        {
-            value: "4",
-            shortValue: "4",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 4,
-        },
-        {
-            value: "5",
-            shortValue: "5",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 5,
-        },
-        {
-            value: "6",
-            shortValue: "6",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 6,
-        },
-        {
-            value: "7",
-            shortValue: "7",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 7,
-        },
-        {
-            value: "8",
-            shortValue: "8",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 8,
-        },
-        {
-            value: "9",
-            shortValue: "9",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 9,
-        },
-        {
-            value: "10",
-            shortValue: "10",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 10,
-        },
-        {
-            value: "jack",
-            shortValue: "J",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 10,
-        },
-        {
-            value: "queen",
-            shortValue: "Q",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 10,
-        },
-        {
-            value: "king",
-            shortValue: "K",
-            suit: "diamond",
-            shortSuit: "♦",
-            price: 10,
-        },
-        {
-            value: "ace",
-            shortValue: "A",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 11,
-        },
-        {
-            value: "2",
-            shortValue: "2",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 2,
-        },
-        {
-            value: "3",
-            shortValue: "3",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 3,
-        },
-        {
-            value: "4",
-            shortValue: "4",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 4,
-        },
-        {
-            value: "5",
-            shortValue: "5",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 5,
-        },
-        {
-            value: "6",
-            shortValue: "6",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 6,
-        },
-        {
-            value: "7",
-            shortValue: "7",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 7,
-        },
-        {
-            value: "8",
-            shortValue: "8",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 8,
-        },
-        {
-            value: "9",
-            shortValue: "9",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 9,
-        },
-        {
-            value: "10",
-            shortValue: "10",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 10,
-        },
-        {
-            value: "jack",
-            shortValue: "J",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 10,
-        },
-        {
-            value: "queen",
-            shortValue: "Q",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 10,
-        },
-        {
-            value: "king",
-            shortValue: "K",
-            suit: "heart",
-            shortSuit: "♥",
-            price: 10,
-        },
-        {
-            value: "ace",
-            shortValue: "A",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 11,
-        },
-        {
-            value: "2",
-            shortValue: "2",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 2,
-        },
-        {
-            value: "3",
-            shortValue: "3",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 3,
-        },
-        {
-            value: "4",
-            shortValue: "4",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 4,
-        },
-        {
-            value: "5",
-            shortValue: "5",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 5,
-        },
-        {
-            value: "6",
-            shortValue: "6",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 6,
-        },
-        {
-            value: "7",
-            shortValue: "7",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 7,
-        },
-        {
-            value: "8",
-            shortValue: "8",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 8,
-        },
-        {
-            value: "9",
-            shortValue: "9",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 9,
-        },
-        {
-            value: "10",
-            shortValue: "10",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 10,
-        },
-        {
-            value: "jack",
-            shortValue: "J",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 10,
-        },
-        {
-            value: "queen",
-            shortValue: "Q",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 10,
-        },
-        {
-            value: "king",
-            shortValue: "K",
-            suit: "spade",
-            shortSuit: "♠",
-            price: 10,
-        },
+        {value: "ace", shortValue: "A", suit: "club", shortSuit: "♣", price: 11, isFlipped: false},
+        {value: "2", shortValue: "2", suit: "club", shortSuit: "♣", price: 2, isFlipped: false},
+        {value: "3", shortValue: "3", suit: "club", shortSuit: "♣", price: 3, isFlipped: false},
+        {value: "4", shortValue: "4", suit: "club", shortSuit: "♣", price: 4, isFlipped: false},
+        {value: "5", shortValue: "5", suit: "club", shortSuit: "♣", price: 5, isFlipped: false},
+        {value: "6", shortValue: "6", suit: "club", shortSuit: "♣", price: 6, isFlipped: false},
+        {value: "7", shortValue: "7", suit: "club", shortSuit: "♣", price: 7, isFlipped: false},
+        {value: "8", shortValue: "8", suit: "club", shortSuit: "♣", price: 8, isFlipped: false},
+        {value: "9", shortValue: "9", suit: "club", shortSuit: "♣", price: 9, isFlipped: false},
+        {value: "10", shortValue: "10", suit: "club", shortSuit: "♣", price: 10, isFlipped: false},
+        {value: "jack", shortValue: "J", suit: "club", shortSuit: "♣", price: 10, isFlipped: false},
+        {value: "queen", shortValue: "Q", suit: "club", shortSuit: "♣", price: 10, isFlipped: false},
+        {value: "king", shortValue: "K", suit: "club", shortSuit: "♣", price: 10, isFlipped: false},
+        {value: "ace", shortValue: "A", suit: "diamond", shortSuit: "♦", price: 11, isFlipped: false},
+        {value: "2", shortValue: "2", suit: "diamond", shortSuit: "♦", price: 2, isFlipped: false},
+        {value: "3", shortValue: "3", suit: "diamond", shortSuit: "♦", price: 3, isFlipped: false},
+        {value: "4", shortValue: "4", suit: "diamond", shortSuit: "♦", price: 4, isFlipped: false},
+        {value: "5", shortValue: "5", suit: "diamond", shortSuit: "♦", price: 5, isFlipped: false},
+        {value: "6", shortValue: "6", suit: "diamond", shortSuit: "♦", price: 6, isFlipped: false},
+        {value: "7", shortValue: "7", suit: "diamond", shortSuit: "♦", price: 7, isFlipped: false},
+        {value: "8", shortValue: "8", suit: "diamond", shortSuit: "♦", price: 8, isFlipped: false},
+        {value: "9", shortValue: "9", suit: "diamond", shortSuit: "♦", price: 9, isFlipped: false},
+        {value: "10", shortValue: "10", suit: "diamond", shortSuit: "♦", price: 10, isFlipped: false},
+        {value: "jack", shortValue: "J", suit: "diamond", shortSuit: "♦", price: 10, isFlipped: false},
+        {value: "queen", shortValue: "Q", suit: "diamond", shortSuit: "♦", price: 10, isFlipped: false},
+        {value: "king", shortValue: "K", suit: "diamond", shortSuit: "♦", price: 10, isFlipped: false},
+        {value: "ace", shortValue: "A", suit: "heart", shortSuit: "♥", price: 11, isFlipped: false},
+        {value: "2", shortValue: "2", suit: "heart", shortSuit: "♥", price: 2, isFlipped: false},
+        {value: "3", shortValue: "3", suit: "heart", shortSuit: "♥", price: 3, isFlipped: false},
+        {value: "4", shortValue: "4", suit: "heart", shortSuit: "♥", price: 4, isFlipped: false},
+        {value: "5", shortValue: "5", suit: "heart", shortSuit: "♥", price: 5, isFlipped: false},
+        {value: "6", shortValue: "6", suit: "heart", shortSuit: "♥", price: 6, isFlipped: false},
+        {value: "7", shortValue: "7", suit: "heart", shortSuit: "♥", price: 7, isFlipped: false},
+        {value: "8", shortValue: "8", suit: "heart", shortSuit: "♥", price: 8, isFlipped: false},
+        {value: "9", shortValue: "9", suit: "heart", shortSuit: "♥", price: 9, isFlipped: false},
+        {value: "10", shortValue: "10", suit: "heart", shortSuit: "♥", price: 10, isFlipped: false},
+        {value: "jack", shortValue: "J", suit: "heart", shortSuit: "♥", price: 10, isFlipped: false},
+        {value: "queen", shortValue: "Q", suit: "heart", shortSuit: "♥", price: 10, isFlipped: false},
+        {value: "king", shortValue: "K", suit: "heart", shortSuit: "♥", price: 10, isFlipped: false},
+        {value: "ace", shortValue: "A", suit: "spade", shortSuit: "♠", price: 11, isFlipped: false},
+        {value: "2", shortValue: "2", suit: "spade", shortSuit: "♠", price: 2, isFlipped: false},
+        {value: "3", shortValue: "3", suit: "spade", shortSuit: "♠", price: 3, isFlipped: false},
+        {value: "4", shortValue: "4", suit: "spade", shortSuit: "♠", price: 4, isFlipped: false},
+        {value: "5", shortValue: "5", suit: "spade", shortSuit: "♠", price: 5, isFlipped: false},
+        {value: "6", shortValue: "6", suit: "spade", shortSuit: "♠", price: 6, isFlipped: false},
+        {value: "7", shortValue: "7", suit: "spade", shortSuit: "♠", price: 7, isFlipped: false},
+        {value: "8", shortValue: "8", suit: "spade", shortSuit: "♠", price: 8, isFlipped: false},
+        {value: "9", shortValue: "9", suit: "spade", shortSuit: "♠", price: 9, isFlipped: false},
+        {value: "10", shortValue: "10", suit: "spade", shortSuit: "♠", price: 10, isFlipped: false},
+        {value: "jack", shortValue: "J", suit: "spade", shortSuit: "♠", price: 10, isFlipped: false},
+        {value: "queen", shortValue: "Q", suit: "spade", shortSuit: "♠", price: 10, isFlipped: false},
+        {value: "king", shortValue: "K", suit: "spade", shortSuit: "♠", price: 10, isFlipped: false}
     ];
-
-    deck = deck.map((card) => ({ ...card, isFlipped: false }));
 
     $(".shuffle").click(function (e) {
         deck.sort(() => 0.5 - Math.random());
-        $(".table").html("");
+        $(".deck").html("");
 
         deck.forEach((card) => {
-            $(".table").append(generateCard(card));
+            $(".deck").append(generateCard(card));
         });
     });
 
     deck.forEach((card) => {
-        $(".table").append(generateCard(card));
+        $(".deck").append(generateCard(card));
+
     });
 
     function generateCard(card) {
-        return `
+        let element = `
         <div class="card ${!card.isFlipped ? "back" : ""} 
         ${card.suit == "club" || card.suit == "spade" ? "black" : "red"}">
             <div class="top" ${!card.isFlipped ? "hidden" : ""}>
@@ -350,6 +85,7 @@ $(document).ready(function () {
             </div>
         </div>
         `;
+        return $(element)
     }
 
     $(".table").on("mousemove", ".card", function (event) {
